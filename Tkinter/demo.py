c90460e1c574183b6f93918841f6350e0ff47442
Prashant Prithyani <prithyanipd@rknec.edu>
2022-05-19 18:26:01 +0530

    .

--- Tkinter/demo.py
+++ Tkinter/demo.py
@@ -1,1 +1,45 @@
-import tkinter
+#importing library
+import tkinter as tk
+ 
+#creating a window 
+win=tk.Tk()
+
+#giving title
+win.title("Demo")
+
+#defining geometry
+win.geometry("960x540")
+
+#initilazing text we want to display
+my_text=tk.Label(win,text="Hey Pack") 
+my_text1=tk.Label(win,text="hey Grid")
+my_text2=tk.Label(win,text="hey ") 
+
+
+#pack method
+my_text.pack(anchor="ne")#by default if will display on center top
+
+#grid method
+#my_text.grid(row=90,column=188)
+
+#place method
+#my_text2.place(x=120,y=150)
+
+#adding buttons
+#creating function for button command
+def button_fun():
+    #print("hey")
+    my_button_label=tk.Label(win,text="hey")
+    my_button_label.pack(anchor="ne")
+
+my_button=tk.Button(win,text="Print Hey",command=button_fun)
+my_button.pack(anchor="ne")
+
+#creating a closing button
+def button_fun2():
+    s
+my_button2=tk.Button(win,text="close",command=)
+
+
+#initilazing loop
+win.mainloop()
